89403e5f2dea3c5a57d1ab722e590e1fca6c32de
Abdellah Maarifa <amaarifa@e1r10p3.1337.ma>
2022-06-12 18:40:29 +0100

    solve some problems style err msg and style promp of signal

--- src/lib/signals.c
+++ src/lib/signals.c
@@ -6,7 +6,7 @@
 /*   By: amaarifa <amaarifa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 11:24:37 by amaarifa          #+#    #+#             */
-/*   Updated: 2022/06/12 11:52:21 by amaarifa         ###   ########.fr       */
+/*   Updated: 2022/06/12 18:36:05 by amaarifa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,21 @@ void	int_handler(int signal)
 
 	//printf("handler 1 worinng with %d\n", g_exit_status);
 	if (signal == SIGINT)
+	{
 		g_exit_status = 1;
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 	else
+	{
 		g_exit_status = 0;
-	printf("\n");
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
+		// printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
 
 void handler_single_cmd(int signal)
